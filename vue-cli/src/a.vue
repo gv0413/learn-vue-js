46470af446210e91b9efbcf68483bd3f0351067b
gv0413 <ggvv0413@gmail.com>
2020-07-22 22:24:39 +0900

    vue setup

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- html -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // js - 인스턴스 옵션
+  methods: {
+    addNum: function() {
+
+    }
+  },
+}
+</script>
+
+<style>
+/* css */
+</style>
