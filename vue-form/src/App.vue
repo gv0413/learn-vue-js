6a1b2913d7375b65f8bb4402c5455c1f70bea48d
gv0413 <ggvv0413@gmail.com>
2020-07-26 00:24:22 +0900

    learn router link

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,45 +1,53 @@
 <template>
-  <form v-on:submit.prevent="submitForm">
-    <div>
-      <label for="username">id: </label>
-      <input type="text" id="username" v-model="username">
-    </div>
-    <div>
-      <label for="password">pw: </label>
-      <input type="password" id="password" v-model="password">
-    </div>
-    <button type="submit" >login</button>
-  </form>
+  <div>
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+    <!-- <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input type="text" id="username" v-model="username">
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input type="password" id="password" v-model="password">
+      </div>
+      <button type="submit" >login</button>
+    </form> -->
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import ToolBar from './components/ToolBar.vue';
+// import axios from 'axios';
 
 export default {
-  data: function() {
-    return {
-      username: '',
-      password: '',
-    }
+  components: {
+    ToolBar
   },
-  methods: {
-    submitForm: function() {
-      // event.preventDefault(); //새로고침을 막아줌
-      console.log(this.username, this.password);
-      var url = 'https://jsonplaceholder.typicode.com/users';
-      var data = {
-        username: this.username,
-        password: this.password
-      }
-      axios.post(url, data)
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function(error){
-          console.log(error);
-        });
-    }
-  },
+  // data: function() {
+  //   return {
+  //     username: '',
+  //     password: '',
+  //   }
+  // },
+  // methods: {
+  //   submitForm: function() {
+  //     // event.preventDefault(); //새로고침을 막아줌
+  //     console.log(this.username, this.password);
+  //     var url = 'https://jsonplaceholder.typicode.com/users';
+  //     var data = {
+  //       username: this.username,
+  //       password: this.password
+  //     }
+  //     axios.post(url, data)
+  //       .then(function(response) {
+  //         console.log(response);
+  //       })
+  //       .catch(function(error){
+  //         console.log(error);
+  //       });
+  //   }
+  // },
 }
 </script>
 
